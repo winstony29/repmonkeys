63f46d756cb1aa2a4272846ccabf2cc5706f01bb
Winston Yang <winstony29@gmail.com>
2025-08-20 01:10:27 +0800

    final ui changes

--- packages/playground/app/layout.tsx
+++ packages/playground/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import '@coinbase/onchainkit/styles.css';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'OnchainKit App - Build the Future On-Chain',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="font-sf-pro">{children}</body>
     </html>
   );
 }
--- packages/playground/tailwind.config.ts
+++ packages/playground/tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     extend: {
       fontFamily: {
         serif: ['Pixelify Sans', 'serif'],
+        'sf-pro': ['-apple-system', 'BlinkMacSystemFont', 'SF Pro Display', 'SF Pro Text', 'system-ui', 'sans-serif'],
       },
       boxShadow: {
         dpad: `
